a041e2610a63a3de500a9cf7b74b7a34680c4408
Austin <maxwellman123@gmail.com>
2020-07-26 20:18:29 -0600

    move output to CAN thread and now WE HAVE LIVE OUTPUT. This may cause thred problems

--- GUI/CandaGUI.py
+++ GUI/CandaGUI.py
@@ -4,17 +4,44 @@ import time as t
 from panda import Panda
 import struct 
 
-UPDATE_FREQUINCY = 30 #hz
+DEBUG = 0
+
+UPDATE_FREQUINCY = 10 #hz
+
+def parse_can_bufferNEW(dat, addresses = None):
+    ret = []
+    for j in range(0, len(dat), 0x10):
+        ddat = dat[j:j + 0x10]
+        f1, f2 = struct.unpack("II", ddat[0:8])
+        extended = 4
+        if f1 & extended:
+            address = f1 >> 3
+        else:
+            address = f1 >> 21
+        if addresses is not None:
+            if address in addresses:
+                dddat = ddat[8:8 + (f2 & 0xF)]
+                if DEBUG:
+                    print(f"  R 0x{address:x}: 0x{dddat.hex()}")
+                ret.append((address, f2 >> 16, dddat, (f2 >> 4) & 0xFF))
+        else:
+            dddat = ddat[8:8 + (f2 & 0xF)]
+            if DEBUG:
+                print(f"  R 0x{address:x}: 0x{dddat.hex()}")
+            ret.append((address, f2 >> 16, dddat, (f2 >> 4) & 0xFF))            
+    return ret
+
+parse_can_buffer = parse_can_bufferNEW
 
 class PandaPlus(Panda):
     def __init__(self, serial=None):
         self._serial = serial
         self._handle = None
     
-    def connect(self, serial=None, claim=True):
+    def start(self, serial=None, claim=True):
         self._serial = serial
         self._handle = None
-        super().connect(claim)    
+        self.connect(claim)    
         
     def can_recv(self, addresses = None): #modified to work with filter can buffer
         dat = bytearray()
@@ -27,22 +54,7 @@ class PandaPlus(Panda):
                 t.sleep(0.1)
         return parse_can_buffer(dat, addresses)
     
-    def parse_can_buffer(dat, addresses = None):
-        ret = []
-        for j in range(0, len(dat), 0x10):
-            ddat = dat[j:j + 0x10]
-            f1, f2 = struct.unpack("II", ddat[0:8])
-            extended = 4
-            if f1 & extended:
-                address = f1 >> 3
-            else:
-                address = f1 >> 21
-            if address in addresses or addresses is None:
-                dddat = ddat[8:8 + (f2 & 0xF)]
-                if DEBUG:
-                    print(f"  R 0x{address:x}: 0x{dddat.hex()}")
-                ret.append((address, f2 >> 16, dddat, (f2 >> 4) & 0xFF))
-        return ret
+
 
 
 
@@ -73,7 +85,7 @@ class output(object):
         self.window.setMeterFill('m' + self.name, "red")
         self.window.setMeterWidth('m' + self.name, 200)
         
-        self.window.addLabel('l' + self.name, "0000000000000000",row,2,0,1)
+        self.lab = self.window.addLabel('l' + self.name, "0000000000000000",row,2,0,1)
         self.window.setLabelBg('l' + self.name, "white")
         self.window.setLabelAlign('l' + self.name, "right")
         self.window.setLabelWidth('l' + self.name, 16)
@@ -82,41 +94,31 @@ class output(object):
         self.window.stopScrollPane()
         
     def set(self, value):
-        self.window.setLabel('l' + self.name, f'{formatBits(value, self.fmt)}')  #remove formmatting remporarly
+        self.lab.config(text=f'{formatBits(value, self.fmt)}')
         #self.window.setMeter('m' + self.name, (value / 0xffffffffffffffff) * 100)
         
 def new():
     name = p.getEntry('name')
     mid = int(p.getEntry('mid'), 16)
     fmt = p.getEntry('fmt')
-    outputs.append(output(p,name, mid, fmt))
-
-
-def distrobuteData(dataBuffer):
-
-    start = t.time()    
-    while len(dataBuffer):
-        MID, _, xdata, bus = dataBuffer.pop()
-        data = struct.unpack('>Q', xdata)[0]
-        for d in outputs: #Data Distrobution betwean the output lines
-            if d.mid == MID:
-                d.set(data)
-        
-    print(t.time() - start, end='\r')    
+    focusMIDs.add(int(mid))
+    outputs.append(output(p,name, mid, fmt))   
                 
 def connectPanda():
     p.showSubWindow("con")
     t.sleep(2)
     try:
         #global dev
-        dev,connect()
+        dev.start()
         p.thread(runCan) 
     except:
         p.setLabel("ConnectStatus", "FAILED! Trying to connect to panda over Wifi")      
+        print("FAILED! Trying to connect to panda over Wifi")      
+                
         try:
             print('Trying panda')
             assert False  #!!!!!!!!!!!!!panda wifi donsn't timout
-            dev = Panda("WIFI")
+            #dev = Panda("WIFI")
         except:
             p.setLabel("ConnectStatus", "Connection timed out!\nClosing in 3 seconds")
             t.sleep(3)
@@ -130,24 +132,33 @@ def connectPanda():
             
             
         
-        
 def simulater():
     ids = [0x120, 0x0e10]
     while 1:
         start = t.time()
         buffer = [(ids[randint(0, 1)], None, struct.pack('>Q',randint(0,0xffffffffffffffff)), 8) for o in range(8000//UPDATE_FREQUINCY)] #get data
-        p.queueFunction(distrobuteData, buffer)
-        print(int(p.eventQueue.qsize))
-        ts = max((1/UPDATE_FREQUINCY)-(t.time() - start), 0)
-        t.sleep(ts)     
+        for MID, _, xdata, bus in buffer:
+            data = struct.unpack('>Q', xdata)[0]
+            for d in outputs: #Data Distrobution betwean the output lines
+                if d.mid == MID:
+                    d.set(data)    
+        dTime = t.time() - start
+        print(dTime, end='\r')
+        t.sleep(max((1/UPDATE_FREQUINCY)-(dTime), 0))
         
 def runCan():
     while 1:
 
         start = t.time()
-        can_recv = dev.can_recv()
-        p.queueFunction(distrobuteData, can_recv)
-        t.sleep(max((1/UPDATE_FREQUINCY)-(t.time() - start), 0))
+        can_recv = dev.can_recv()#focusMIDs)
+        for MID, _, xdata, bus in can_recv:
+            data = struct.unpack('>Q', xdata)[0]
+            for d in outputs: #Data Distrobution betwean the output lines
+                if d.mid == MID:
+                    d.set(data)      
+        dTime = t.time() - start
+        print(dTime, end='\r')
+        t.sleep(max((1/UPDATE_FREQUINCY)-(dTime), 0))
 
         
 
@@ -180,6 +191,8 @@ p.stopFrame()
 outputs = []
 p.startFrame('bottom', row=1, colspan=2)
 
+focusMIDs = set()
+
 p.addLabelEntry("name", 0,0)
 p.addLabelEntry("mid", 0,1)
 p.addLabelEntry("fmt", 0,2)
@@ -192,7 +205,7 @@ p.setEntry("mid", '10')
 p.setEntry("fmt", "16:15")
 new()
 p.setEntry("name", 'Ttgst')
-p.setEntry("mid", '0e1e')
+p.setEntry("mid", '10')
 p.setEntry("fmt", "32:15")
 new()
 p.addButton("Make Value", new, 0,3)
